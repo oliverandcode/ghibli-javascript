f66c9c975c1b2c39eb6cbf2114709ed7dcfdccbc
Oliver Ayers <oliverdayers@gmail.com>
2020-08-25 15:25:25 -0700

    started scripts

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// SOURCE: https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
+
+// Create a request variable and assign a new XMLHttpRequestobject to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach((movie) => {
+            console.log(movie.title)
+        })
+    } else {
+        console.log('error')
+    }
+}
+
+// Send request
+request.send()
